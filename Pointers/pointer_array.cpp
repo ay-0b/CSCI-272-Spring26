8ada3f39a6d731e9d8ff7e6d5324999efbef1ee1
Ayoub Rannan <ayoubrannan42@gmail.com>
2026-08-20 22:24:58 -0400

    Fix pointer_array.cpp: allocate before dereferencing in first loop

--- Pointers/pointer_array.cpp
+++ Pointers/pointer_array.cpp
@@ -4,12 +4,11 @@ using namespace std;
 int main(){
     int *p[10];
     for (int i=0; i<10; ++i){
+        p[i] = new int;
         *p[i] = i;
         cout << *p[i] << endl;
     }
     for (int i = 0; i < 10; ++i){
-        p[i] = new int;
-        *p[i] = i;
         cout << p[i] << endl;
     }
 
